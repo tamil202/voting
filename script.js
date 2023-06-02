f2422ac2628b07fc4d5bb1a98549cdd40dc9ec2f
TamilVanan <110275311+tamil202@users.noreply.github.com>
2023-06-02 15:15:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function validate() {
+  var username = document.getElementById("userName").value;
+  var password = document.getElementById("password").value;
+
+  // Validate username
+  if (!username) {
+    alert("Please enter a username.");
+    return false;
+  }
+
+  // Validate password
+  if (!password) {
+    alert("Please enter a password.");
+    return false;
+  }
+
+  // Additional validation rules for username and password
+  // Add your own validation logic here
+
+  // Validation successful
+  //
+
+  let Uname = "batch8";
+    let password1 = "parties1";
+
+    if (username == Uname) {
+        if (password == password1) {
+            window.location = "http://127.0.0.1:5500/vote.html";
+        }
+    }
+    
+
+  alert("Validation wrong!");
+      return true;
+}
